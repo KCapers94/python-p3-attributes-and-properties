cbe283fa4eb69d8e770fd0421db7289c2549d2d8
Keith Capers <K.Capers94@gmail.com>
2024-09-30 19:11:21 -0400

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,44 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+
+    APPROVED_JOBS = [
+    "Admin",
+    "Customer Service",
+    "Human Resources",
+    "ITC",
+    "Production",
+    "Legal",
+    "Finance",
+    "Sales",
+    "General Management",
+    "Research & Development",
+    "Marketing",
+    "Purchasing"
+  ]
+
+    def __init__(self,name = "Person",job = "Purchasing"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self): 
+        return self._name
+ 
+    @property
+    def job(self):
+        return self._job
+    
+    @name.setter    
+    def name(self, value):
+        if isinstance(value,str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print( "Job must be in list of approved jobs." )
